e8e3fd5ae7d37c10f2deef18c18adfa18d628984
Preston Riley <123032424+Preston-Riley@users.noreply.github.com>
2024-04-07 17:11:52 -0700

    added files

---
+++ config.js
@@ -0,0 +1,20 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
+  VITE_CORS_PROXY_URL: "https://heroic-meringue-26c962.netlify.app/",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmE3NGM5MWYyM2UxNGQxMzdiMDc4ZGE5NDQyNDU0NCIsInN1YiI6IjY1Y2E2NDU0NTRhMDk4MDE2MzAyOTkxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JSYLVBWW3zOCPM9PydAZ0xqsc5AWmfElCubXYfU4YmQ",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: "pres1234569@gmail.com",
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with
+  VITE_BACKEND_URL: null,
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: "",
+};
